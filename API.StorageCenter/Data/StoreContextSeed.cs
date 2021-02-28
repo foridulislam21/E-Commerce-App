3c94da3b4305359a5a7cca7a5d5845804c1b172a
foridulislam21 <foridulislam21@gmail.com>
2021-03-01 01:09:24 +0600

    working with deploying at cloud server

--- API.StorageCenter/Data/StoreContextSeed.cs
+++ API.StorageCenter/Data/StoreContextSeed.cs
@@ -2,11 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text.Json;
 using System.Threading.Tasks;
 using API.Models;
 using API.Models.OrderAggregate;
-using API.StorageCenter.Data;
 using Microsoft.Extensions.Logging;
 
 namespace API.StorageCenter.Data
@@ -17,9 +17,10 @@ namespace API.StorageCenter.Data
         {
             try
             {
+                var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
                 if (!context.ProductBrands.Any())
                 {
-                    var brandsData = File.ReadAllText("../API.Models/SeedData/brands.json");
+                    var brandsData = File.ReadAllText(path + @"/Data/SeedData/brands.json");
                     var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
 
                     foreach (var item in brands)
@@ -31,7 +32,7 @@ namespace API.StorageCenter.Data
 
                 if (!context.ProductTypes.Any())
                 {
-                    var typessData = File.ReadAllText("../API.Models/SeedData/types.json");
+                    var typessData = File.ReadAllText(path + @"/Data/SeedData/types.json");
                     var types = JsonSerializer.Deserialize<List<ProductType>>(typessData);
 
                     foreach (var item in types)
@@ -43,7 +44,7 @@ namespace API.StorageCenter.Data
 
                 if (!context.Products.Any())
                 {
-                    var productsData = File.ReadAllText("../API.Models/SeedData/products.json");
+                    var productsData = File.ReadAllText(path + @"/Data/SeedData/products.json");
                     var products = JsonSerializer.Deserialize<List<Product>>(productsData);
 
                     foreach (var item in products)
@@ -54,7 +55,7 @@ namespace API.StorageCenter.Data
                 }
                 if (!context.DeliveryMethods.Any())
                 {
-                    var dmData = File.ReadAllText("../API.Models/SeedData/delivery.json");
+                    var dmData = File.ReadAllText(path + @"/Data/SeedData/delivery.json");
                     var delivery = JsonSerializer.Deserialize<List<DeliveryMethod>>(dmData);
 
                     foreach (var item in delivery)
--- API/Program.cs
+++ API/Program.cs
@@ -45,7 +45,8 @@ namespace API
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                    .UseUrls("http://localhost:5011");
                 });
     }
 }
